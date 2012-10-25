29e1e3c260f85bea11c5cc2cf9fea1037bf5854d
bella24988 <megliodituo11@hotmail.com>
2012-10-26 00:37:41 +0200

    annulla ordine fatto

---
+++ guanabana/src/client/AnnullaOrdineController.java
@@ -0,0 +1,35 @@
+package client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class AnnullaOrdineController implements ActionListener{
+	
+	private AnnullaOrdinePanel annullaOrdinePanel;
+
+	
+	public AnnullaOrdineController(AnnullaOrdinePanel aPanel) {
+		super();
+		this.annullaOrdinePanel = aPanel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if (e.getActionCommand().equalsIgnoreCase("Si")){
+			Client servizioClient = new Client();
+			try {
+				servizioClient.aggiornaOrdine(annullaOrdinePanel.getOrdine().getNumeroOrdine(),"ANNULLATO");
+				annullaOrdinePanel.getContenutoPanel().mostraRingraziamento();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else if (e.getActionCommand().equalsIgnoreCase("No")){
+			annullaOrdinePanel.getContenutoPanel().mostraRingraziamento();
+		}
+	}
+
+	
+}
