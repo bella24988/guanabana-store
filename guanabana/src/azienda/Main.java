1bb870fd872b8a02fe96cc629924d16fb3898a02
VERONICA MELENDEZ <megliodituo11@hotmail.com>
2012-11-10 11:58:11 +0100

    prova

--- guanabana/src/azienda/Main.java
+++ guanabana/src/azienda/Main.java
@@ -3,13 +3,6 @@ package azienda;
 import java.awt.EventQueue;
 
 public class Main {
-
-	/**
-	 * @param args
-	 */
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
